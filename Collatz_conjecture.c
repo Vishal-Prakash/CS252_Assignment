e8c2730ea622ec3fc1a3706ee3491c36cf49160d
Vishal-Prakash <72442915+Vishal-Prakash@users.noreply.github.com>
2021-11-22 22:13:40 +0530

    Small changes and commented the code
    Changed variable name k to num. Displayed pid of parent and child. Commented the code. Other minor changes.

--- Collatz_conjecture.c
+++ Collatz_conjecture.c
@@ -1,51 +1,81 @@
+//P Vishal
+//191EE139
+//CS252 Assignment
+
+/* Q3.21 
+C program to generate the Collatz Conjecture for a given starting number.
+*/
+
+
+//Required libraries
 #include <stdio.h>
 #include <sys/wait.h>
 #include <unistd.h>
 #include <stdlib.h>
 
 
+//Main function
 int main(int argc, char *argv[])
 {
-	printf("\n\nCollatz Conjecture\n");
+	printf("\n\n        Collatz Conjecture\n");
 
-	int n=0;
-	int k= atoi (argv[1]);
+	//Local variable declaration
+    int n=0,num;
 	
 	pid_t pid;
 
-		/*do
-		{
-			printf("Please enter a number greater than 0 to run the Collatz Conjecture.\n"); 
-  			scanf("%d", &k);	
-		}while (k <= 0);
-*/
-		pid = fork();
+    if (argc == 1)
+    {
+        printf("\nERROR: First number of the Collatz conjecture sequence not entered \n Please enter a positive integer the command line\n\n");
+        exit(1);
+    }
+    else if (argc == 2)
+    {
+        if(atoi(argv[1]) <= 0) //Error Checking
+        {
+            printf("\nInvalid Input \n Please enter a positive integer\n\n");
+            exit(1);
+        }
+        num = atoi (argv[1]);
+        printf("\nThe entered first number is: %d\n", num);
+    }
 
-		if (pid == 0)
+
+	//Process ID of parent
+    printf("\nParent process created having pid %d\n", (int) getpid());
+        
+    //Forking a child process
+    pid = fork();
+
+	if (pid == 0) //child process
 		{
-			printf("Child is working...\n");
-			printf("%d\n",k);
-			while (k!=1)
+			printf("\nChild process created having pid %d\n",(int) getpid());
+            printf("\nChild is working...\n");
+            //Finding the Collatz conjecture
+            printf("\nThe Collatz Conjecture Sequence is as follows:\n");
+			printf("%d  ",num);
+			while (num!=1)
 			{
-				if (k%2 == 0)
+				if (num%2 == 0)
 				{
-					k = k/2;
+					num = num/2;
 				}
-				else if (k%2 == 1)
+				else if (num%2 == 1)
 				{
-					k = 3 * (k) + 1;
+					num = 3 * (num) + 1;
 				}	
 			
-				printf("%d\n",k);
+				printf("%d  ",num);
 			}
 		
-			printf("Child process is done.\n");
+			printf("\nChild process is done.\n");
 		}
-		else
+		else //Parent process
 		{
-			printf("Parents is waiting on child process...\n");
+			printf("\nParents is waiting on child process...\n\n");
 			wait(&n);
-			printf("Parent process is done.\n");
+            //Parent process waiting for child process to complete after which it will get executed
+			printf("\nParent process is done.\n");
 		}
 	return 0; 
 }
